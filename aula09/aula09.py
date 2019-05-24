06272a1e53642f7471e66e820ded8d8982c26de3
marcos <gomes.marcosjf@gmail.com>
2019-05-24 10:28:33 -0300

    update ex22 solved

--- aula09/aula09.py
+++ aula09/aula09.py
@@ -30,31 +30,35 @@ print(frase[15:]) # Fatiamento: substring do índice 15 até o final
 print(frase[9::3]) # Fatiamento: substring do índice 9 até o final, pulando de 3 em 3'''
 
 # Análise
-print(len(frase)) #Tamanho da string
-print(frase.count('o')) #Quantas vezes aparece o char 'o'
-print(frase.count('o', 0 , 13)) #Quantas vezes aparece o char 'o' entre o índice 0 a 12
-print(frase.find('deo')) #Em qual índice aparece a substring 'deo'
-print(frase.find('Android')) #Substring inexistente retorna o valor -1
-print('Curso' in frase) #Verifica se a substring existe na string (True/False)
+print(len(frase))  # Tamanho da string
+print(frase.count('o'))  # Quantas vezes aparece o char 'o'
+# Quantas vezes aparece o char 'o' entre o índice 0 a 12
+print(frase.count('o', 0, 13))
+print(frase.find('deo'))  # Em qual índice aparece a substring 'deo'
+print(frase.find('Android'))  # Substring inexistente retorna o valor -1
+print('Curso' in frase)  # Verifica se a substring existe na string (True/False)
 
 # Transformação
-print(frase.replace('Python', 'Android')) #Substitui a primeira substring pela segunda
+# Substitui a primeira substring pela segunda
+print(frase.replace('Python', 'Android'))
 print(frase.upper())
 print(frase.lower())
 print(frase.capitalize())
-print(frase.title()) #Analisa quantas palavras a string possui e torna a primeira letra de cada maiúscula
+# Analisa quantas palavras a string possui e torna a primeira letra de cada maiúscula
+print(frase.title())
 
 frase = '   Aprenda Python  '
-print(frase.strip()) #Remove os espaços no início e no final da string
-print(frase.rstrip()) #Remove os espaços ndireita da string
-print(frase.lstrip()) #Remove os espaços nna esquerda da string
+print(frase.strip())  # Remove os espaços no início e no final da string
+print(frase.rstrip())  # Remove os espaços direita da string
+print(frase.lstrip())  # Remove os espaços na esquerda da string
 
 # Divisão
 frase = 'Curso em Vídeo Python'
-print(frase.split()) #Divide a string, tendo como parâmetro padrão os espaços entre as letras,
-                     # em uma lista de seus "pedaços" a partir dos espaços 
+# Divide a string, tendo como parâmetro padrão os espaços entre as letras,
+print(frase.split())
+# em uma lista de seus "pedaços" a partir dos espaços
 frase = frase.split()
-print('.'.join(frase)) #Adiciona o caractere '.' entre cada índice da string
+print('.'.join(frase))  # Adiciona o caractere '.' entre cada índice da string
 
 ############PRÁTICA DA AULA 09################
 frase = 'Curso em Vídeo Python'
--- aula09/ex022.py
+++ aula09/ex022.py
@@ -1,5 +1,14 @@
-#Ler o nome completo de uma pessoa e:
-#printar o nome com todas as letras maiúsculas
-#printar o nome com todas as letras minúsculas
-#printar quantas letras ao todo (sem considerar os espaços)
-#printar quantas letras tem o primeiro nome
+# Ler o nome completo de uma pessoa e:
+# printar o nome com todas as letras maiúsculas
+# printar o nome com todas as letras minúsculas
+# printar quantas letras ao todo (sem considerar os espaços)
+# printar quantas letras tem o primeiro nome
+
+nome = input('Informe seu nome completo: ')
+print('Nome em maiúsculo: {}'.format(nome.upper()))
+print('Nome em minúsculo: {}'.format(nome.lower()))
+nomeSplit = nome.split()
+nomeSplit = ''.join(nomeSplit)
+print('Quantidade de letras: {}'.format(len(nome)))
+print('Quantidade de letras sem contar espaços: {}'.format(len(nomeSplit)))
+print('Quantidade de letras no primeiro nome: {}'.format(len(nome.split()[0])))
